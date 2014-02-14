354e7a616f6ab17e738783f56a6c292e9d77d264
wmak <william.mak@hotmail.ca>
2014-02-13 22:22:42 -0500

    moving the partition files to a src folder

---
+++ src/raptor/partition/partition_test.go
@@ -0,0 +1,22 @@
+package partition
+
+import (
+	"raptor/partition"
+	"testing"
+)
+
+func TestPartition(t *testing.T) {
+	IL, IS, JL, JS := partition.Partition(10, 20)
+	if IL != 1 {
+		t.Error("IL calculated improperly")
+	}
+	if IS != 0 {
+		t.Error("IS calculated improperly")
+	}
+	if JL != 10 {
+		t.Error("JL calculated improperly")
+	}
+	if JS != 10 {
+		t.Error("JS calculated improperly")
+	}
+}
