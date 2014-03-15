b4f22d0e7e13467e4c26f28c27e3e7bb2f4b17d8
David Kua <david@kua.io>
2014-03-15 00:30:28 -0400

    Replaced string arg with io.Reader in Block().
    Requested fix for Issue #2.
    Block() function definition needed to be changed since getting the
    filesize was no longer the responsibility of Block() but of the caller.
    
    If there is a better way without doing it would be nice.

--- encoder/encoder_test.go
+++ encoder/encoder_test.go
@@ -1,6 +1,8 @@
 package encoder
 
 import (
+	"log"
+	"os"
 	"testing"
 )
 
@@ -25,7 +27,16 @@ This is more a sanity test than a proper test, check that the blocks have
 different symbols. and that all the symbols aren't the same
 */
 func TestEncoder(t *testing.T) {
-	result := Block("test")
+	file, err := os.Open("test")
+	if err != nil {
+		log.Fatal(err)
+	}
+	info, err := os.Stat("test")
+	if err != nil {
+		log.Fatal(err)
+	}
+	size := info.Size()
+	result := Block(file, size)
 	blockA := result.blocks[0]
 	blockB := result.blocks[1]
 	if blockA.symbols[1].dat[1] == blockB.symbols[1].dat[1] {
